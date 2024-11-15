fb1cf4870e72a23ff2ff2cad35cb34945c2b3ed7
Sidiki929 <dsidiki@jway.ma>
2024-11-15 18:01:59 +0100

    Fixed ink

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const API_URL = `${env.VITE_APP_BASE_NAME}`;
-  // this sets a default port to 3000
-  const PORT = `${'3000'}`;
+  const API_URL = env.VITE_API_URL || 'kalycee'; // fallback to default if not defined
+  const PORT = env.VITE_PORT || '3000';
 
   return {
     server: {
-      // this ensures that the browser opens upon server start
       open: true,
       port: PORT
     },
@@ -18,19 +17,18 @@ export default defineConfig(({ mode }) => {
       global: 'window'
     },
     resolve: {
-      //     alias: [
-      //       {
-      //         find: /^~(.+)/,
-      //         replacement: path.join(process.cwd(), 'node_modules/$1')
-      //       },
-      //       {
-      //         find: /^src(.+)/,
-      //         replacement: path.join(process.cwd(), 'src/$1')
-      //       }
-      //     ]
+      alias: [
+        {
+          find: /^~(.+)/,
+          replacement: path.join(process.cwd(), 'node_modules/$1')
+        },
+        {
+          find: /^src(.+)/,
+          replacement: path.join(process.cwd(), 'src/$1')
+        }
+      ]
     },
     preview: {
-      // this ensures that the browser opens upon preview start
       open: true,
       port: PORT
     },
